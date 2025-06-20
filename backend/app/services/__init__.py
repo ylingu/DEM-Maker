004d5454aab35690f00bf4cc78bd2a43d044c8f6
lhghfhdbdb <22374193@buaa.edu.cn>
2025-06-20 12:42:30 +0800

    Add files via upload

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,10 +1,12 @@
 from .drone_service import DroneCommand, DroneService
 from .record_service import RecordService
 from .stream_service import StreamService
+from .ply_service import PlyService
 
 __all__ = [
     "DroneCommand",
     "DroneService", 
     "RecordService",
     "StreamService",
+    "PlyService",
 ]
